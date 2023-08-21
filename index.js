dd1f27f73b5b2099ae20b4cdd465c730478e018b
Sourish Sarmah <sourishsarmah@gmail.com>
2023-08-22 00:12:44 +0530

    added text fields and post API for accepting user data

--- index.js
+++ index.js
@@ -5,17 +5,25 @@ const port = 3000;
 
 
 app.use(express.static(path.join(__dirname, 'public')));
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({
+    extended: true
+}));
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
 
 
-app.get('/user', (req, res) => {
+app.post('/user', (req, res) => {
+    console.log(req.body.user)
     res.json({ message: 'Hello from the server!' });
-  });
-  
+});
+
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
--- public/app.js
+++ public/app.js
@@ -1,5 +1,22 @@
 document.getElementById('btn').addEventListener('click', async () => {
-    const response = await fetch('/user');
-    const data = await response.json();
-    alert(data.message);
+  let userName = document.getElementById("username").value
+  let userEmail = document.getElementById("email").value
+  let requestBody = {
+    name: userName,
+    email: userEmail
+  }
+  alert(JSON.stringify(requestBody))
+  const response = await fetch('/user', {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      "user": {
+        "name": userName,
+        "email": userEmail
+      }
+    })
   });
+
+  const data = await response.json();
+  alert(username);
+});
